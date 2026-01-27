70f25c0e609682c6cd271d5a2ab9706d82e92b80
Akhil Krishnan G <akhilrohini@gmail.com>
2026-01-27 21:03:09 +0530

    Landing Page + Logo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideLottieOptions({
+      player: () => player,
+    }),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LottieComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('the-happy-hive');
+  options: AnimationOptions = {
+    path: '/lotties/thh_loader.json',
+    loop: false,
+  };
 }

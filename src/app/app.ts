ce10c347b8968fc9c5bdbe249cfb02f77873a5b7
Akhil Krishnan G <akhilrohini@gmail.com>
2026-02-23 13:18:39 +0530

    Dashbard updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LottieComponent, AnimationOptions } from 'ngx-lottie';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [LottieComponent],
+  imports: [LottieComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('the-happy-hive');
+  protected showLoader = signal(true);
   options: AnimationOptions = {
     path: '/lotties/thh_loader.json',
     loop: false,
   };
+
+  ngOnInit() {
+
+    
+
+    setTimeout(() => {
+      this.showLoader.set(false);
+    }, 4000);
+  }
 }
